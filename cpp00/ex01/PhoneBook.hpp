aeec5cd856b912d51354a3748609bc6a44af3e17
Nicolas Premont <ni.premont@gmail.com>
2024-05-29 14:03:42 +0200

    init + cpp00ex00 done

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 14:06:22 by npremont          #+#    #+#             */
+/*   Updated: 2024/05/29 12:37:04 by npremont         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+#include "Contact.hpp"
+
+class PhoneBook {
+
+private:
+	Contact _contacts[8];
+		
+};
+
+#endif
